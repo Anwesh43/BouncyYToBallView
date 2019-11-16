d32e9fec0755fb05e2614fda76a4dfe8f1761246
Anwesh43 <anweshthecool0@gmail.com>
2019-11-16 11:42:22 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedbouncyytoballview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbouncyytoballview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedbouncyytoballview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.bouncyytoballview.BouncyYToBallView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BouncyYToBallView.create(this)
     }
 }
