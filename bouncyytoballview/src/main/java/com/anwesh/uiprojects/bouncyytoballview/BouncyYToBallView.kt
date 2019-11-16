97b050b9249613fe7db76688deeebfe317b4600a
Anwesh43 <anweshthecool0@gmail.com>
2019-11-16 11:11:21 +0530

    adding constants and libraries

--- bouncyytoballview/src/main/java/com/anwesh/uiprojects/bouncyytoballview/BouncyYToBallView.kt
+++ bouncyytoballview/src/main/java/com/anwesh/uiprojects/bouncyytoballview/BouncyYToBallView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.bouncyytoballview
 /**
  * Created by anweshmishra on 16/11/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 3
+val scGap : Float = 0.02f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#2196F3")
+val backColor : Int = Color.parseColor("#BDBDBD")
