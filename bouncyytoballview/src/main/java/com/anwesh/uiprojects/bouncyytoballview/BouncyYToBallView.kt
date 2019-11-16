7bf294f973e429377f8407a2e949f2afb727444a
Anwesh43 <anweshthecool0@gmail.com>
2019-11-16 11:31:23 +0530

    adding Animator class

--- bouncyytoballview/src/main/java/com/anwesh/uiprojects/bouncyytoballview/BouncyYToBallView.kt
+++ bouncyytoballview/src/main/java/com/anwesh/uiprojects/bouncyytoballview/BouncyYToBallView.kt
@@ -20,6 +20,7 @@ val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#2196F3")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val rFactor : Float = 2.4f
+val delay : Long = 30
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -86,6 +87,34 @@ class BouncyYToBallView(ctx : Context) : View(ctx) {
             if (dir == 0f) {
                 dir = 1f - 2 * prevScale
                 cb()
+            }
+        }
+    }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(delay)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
             }
         }
     }
